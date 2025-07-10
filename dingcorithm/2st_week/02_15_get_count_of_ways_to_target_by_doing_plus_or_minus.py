a036b09ef8ab9e1edd6dd88b8ccb6229c9e86564
ryeokpark <ryeokpark@aideep.ai>
2025-07-10 09:01:34 +0900

    이진탐색 재귀함수 숙제

--- dingcorithm/2st_week/02_15_get_count_of_ways_to_target_by_doing_plus_or_minus.py
+++ dingcorithm/2st_week/02_15_get_count_of_ways_to_target_by_doing_plus_or_minus.py
@@ -1,9 +1,18 @@
-numbers = [1, 1, 1, 1, 1]
-target_number = 3
+
+
+
+numbers = [2, 3, 1]
+target_number = 0
+# numbers = [1, 1, 1, 1, 1]
+# target_number = 3
+
+result = []
 
 
 def get_count_of_ways_to_target_by_doing_plus_or_minus(array, target):
-    # 구현해보세요!
+
+
+
     return 5
 
 
