d730926dd2fc921f83be1de1ab23d7bb3c1bfd82
pryeok <pryeok@gmail.com>
2025-07-23 22:56:21 +0900

    HEAP BFS DFS DP 숙제

--- dingcorithm/4st_week/04_08_get_minimum_count_of_overseas_supply.py
+++ dingcorithm/4st_week/04_08_get_minimum_count_of_overseas_supply.py
@@ -1,11 +1,15 @@
 
 
-
 import heapq
 
+# ramen_stock = 4
+# supply_dates = [4, 10, 15]
+# supply_supplies = [20, 5, 10]
+# supply_recover_k = 30
+
 ramen_stock = 4
-supply_dates = [4, 10, 15]
-supply_supplies = [20, 5, 10]
+supply_dates = [1, 7, 11]
+supply_supplies = [10, 20, 30]
 supply_recover_k = 30
 
 
@@ -16,8 +20,10 @@ def get_minimum_count_of_overseas_supply(stock, dates, supplies, k):
     max_heap = []
 
     while stock <= k:
+        print("stock =", stock)
         while last_added_date_index < len(dates) and dates[last_added_date_index] <= stock:
             heapq.heappush(max_heap, -supplies[last_added_date_index])
+            print("max_heap : ", max_heap)
             last_added_date_index += 1
 
         answer += 1
