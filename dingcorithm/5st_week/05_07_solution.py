c2807966220368404d3cd44b6d32161775aea4f3
pryeok <pryeok@gmail.com>
2025-07-30 21:49:58 +0900

    카카오 추가 코딩 테스트 - 크레인 인형뽑기 게임

--- dingcorithm/5st_week/05_07_solution.py
+++ dingcorithm/5st_week/05_07_solution.py
@@ -14,13 +14,11 @@ moves = [1,5,3,5,1,2,1,4]
 #   4      3       1        1 3 2 x 4
 # [3][0] [1][4]  [1][2]
 
-
+# 내가 풀어본 문제
 def solution(board, moves):
-
     stack = []
     result_count = 0
     cur = 0
-
     for i in moves:
         for j in range(len(board)):
             if board[j][i-1] == 0:
@@ -33,8 +31,21 @@ def solution(board, moves):
                     stack.append(board[j][i-1])
                 board[j][i - 1] = 0
                 break
-
     return result_count
 
+# 해설
+def solution(board, moves):
+    bucket = []
+    answer = 0
+
+    for move in moves:
+        index = move - 1
+        for row_info in board:
+            if row_info[index] != 0:
+                bucket.append(row_info[index])
+                row_info[index] = 0
+                break
+
+    return answer
 
 print("정답 = 4 / 현재 풀이 값 = ", solution(board, moves))
